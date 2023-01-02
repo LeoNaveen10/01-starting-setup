b026dcd5e86b6efe95021bfacbeebab443825198
LeoNaveen10 <naveensundar313@gmail.com>
2023-01-02 13:20:25 +0530

    react components part 3 done

--- src/components/ExpensesSpread.js
+++ src/components/ExpensesSpread.js
@@ -1,32 +1,32 @@
-import './ExpensesSpread.css'
+import './ExpensesSpread.css';
 import ExpenseReport from './ExpenseReport';
+import Card from './Card';
 
 function ExpenseSpread(props) {
-    return (
-			<div className="expenses">
-				<ExpenseReport
-					title={props.expenses[0].title}
-					amount={props.expenses[0].amount}
-					date={props.expenses[0].date}
-				/>
-				<ExpenseReport
-					title={props.expenses[1].title}
-					amount={props.expenses[1].amount}
-					date={props.expenses[1].date}
-				/>
-				<ExpenseReport
-					title={props.expenses[2].title}
-					amount={props.expenses[2].amount}
-					date={props.expenses[2].date}
-				/>
-				<ExpenseReport
-					title={props.expenses[3].title}
-					amount={props.expenses[3].amount}
-					date={props.expenses[3].date}
-				/>
-			</div>
-		);
-   
+	return (
+		<Card className='expenses'>
+			<ExpenseReport
+				title={props.expenses[0].title}
+				amount={props.expenses[0].amount}
+				date={props.expenses[0].date}
+			/>
+			<ExpenseReport
+				title={props.expenses[1].title}
+				amount={props.expenses[1].amount}
+				date={props.expenses[1].date}
+			/>
+			<ExpenseReport
+				title={props.expenses[2].title}
+				amount={props.expenses[2].amount}
+				date={props.expenses[2].date}
+			/>
+			<ExpenseReport
+				title={props.expenses[3].title}
+				amount={props.expenses[3].amount}
+				date={props.expenses[3].date}
+			/>
+		</Card>
+	);
 }
 
 export default ExpenseSpread;
